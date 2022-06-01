7be62e3ee210167a590696ab0de3b496684bae0e
youssefhesham2 <youssef.hesham.rabea@gmail.com>
2022-06-01 23:18:35 +0200

    Add main activity presenter and view

--- app/src/main/java/com/example/instabugintern2022/mainactivity/MainActivity.kt
+++ app/src/main/java/com/example/instabugintern2022/mainactivity/MainActivity.kt
@@ -1,31 +1,56 @@
 package com.example.instabugintern2022.mainactivity
 
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.forEach
+import com.example.data.datasources.DatabaseHelper
+import com.example.data.repositories.MainRepositoryImpl
+import com.example.domain.entities.HeaderEntity
+import com.example.domain.entities.ResponseEntity
+import com.example.domain.usecases.MainUseCase
+import com.example.instabugintern2022.History.HistoryActivity
 import com.example.instabugintern2022.R
 import com.example.instabugintern2022.databinding.ActivityMainBinding
+import com.example.instabugintern2022.displayactivity.DisplayActivity
+import com.google.android.material.snackbar.Snackbar
 
 
-class MainActivity constructor(private val presenter: MainPresenter = MainPresenter()) :
-    AppCompatActivity(), MainView {
+class MainActivity : AppCompatActivity(), MainView {
+    private lateinit var presenter: MainPresenter
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        presenter =
+            MainPresenter(MainUseCase(MainRepositoryImpl(databaseHelper = DatabaseHelper(this))))
 
         presenter.onAttached(this)
-        onClickAddNewHeaderBtn()
+        setupOnClickAddNewParamBtn()
+        setupOnClickAddNewHeaderBtn()
         setupSpinner()
+        setupOnClickSendRequestBtn()
+        setupOnClickHistoryBtn()
     }
 
-    private fun onClickAddNewHeaderBtn() {
+    private fun setupOnClickAddNewParamBtn() {
+        binding.btnAddNewParam.setOnClickListener(View.OnClickListener {
+            presenter.onClickAddNewParamBtn()
+        })
+    }
+
+    private fun setupOnClickAddNewHeaderBtn() {
         binding.btnAddNewHeader.setOnClickListener(View.OnClickListener {
             presenter.onClickAddNewHeaderBtn()
         })
@@ -36,7 +61,7 @@ class MainActivity constructor(private val presenter: MainPresenter = MainPresen
             object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(
                     parent: AdapterView<*>,
-                    view: View,
+                    view: View?,
                     position: Int,
                     id: Long
                 ) {
@@ -47,7 +72,13 @@ class MainActivity constructor(private val presenter: MainPresenter = MainPresen
             }
     }
 
-    private fun checkUserInternetConnection(): Boolean {
+    private fun setupOnClickSendRequestBtn() {
+        binding.btnSendRequest.setOnClickListener {
+            presenter.onClickSendRequestBtn()
+        }
+    }
+
+    override fun getUserInternetConnectionState(): Boolean {
         val connectivityManager =
             this.getSystemService(Context.CONNECTIVITY_SERVICE) as (ConnectivityManager)
         if (Build.VERSION.SDK_INT < 23) {
@@ -65,9 +96,62 @@ class MainActivity constructor(private val presenter: MainPresenter = MainPresen
         return false
     }
 
+    override fun getParamListFromLayout(): ArrayList<HeaderEntity> {
+        val params = ArrayList<HeaderEntity>()
+        binding.paramForm.forEach {
+            val value = it.findViewById<EditText>(R.id.tv_header_value)
+            val key = it.findViewById<EditText>(R.id.tv_header_key)
+            params.add(HeaderEntity(key.text.toString(), value.text.toString()))
+        }
+        return params
+    }
+
+    override fun getHeaderListFromLayout(): ArrayList<HeaderEntity> {
+        val headers = ArrayList<HeaderEntity>()
+        binding.headerForm.forEach {
+            val value = it.findViewById<EditText>(R.id.tv_header_value)
+            val key = it.findViewById<EditText>(R.id.tv_header_key)
+            headers.add(HeaderEntity(key.text.toString(), value.text.toString()))
+        }
+        return headers
+    }
+
+    private fun setupOnClickHistoryBtn() {
+        binding.fabHistory.setOnClickListener {
+            presenter.onClickHistoryBtn()
+        }
+    }
+
+    override fun goneParamViewVisibility() {
+        binding.paramLayout.visibility = View.GONE
+    }
+
+    override fun visibleParamViewVisibility() {
+        binding.paramLayout.visibility = View.VISIBLE
+    }
+
+    override fun addNewParamField() {
+        val paramItem = layoutInflater.inflate(R.layout.header_item, null)
+        binding.paramForm.addView(paramItem)
+        paramItem.findViewById<ImageView>(R.id.img_delete_header_field).setOnClickListener {
+            binding.paramForm.removeView(paramItem)
+        }
+    }
+
     override fun addNewHeaderField() {
         val headerItem = layoutInflater.inflate(R.layout.header_item, null)
-        binding.headerLayout.addView(headerItem)
+        binding.headerForm.addView(headerItem)
+        headerItem.findViewById<ImageView>(R.id.img_delete_header_field).setOnClickListener {
+            binding.headerForm.removeView(headerItem)
+        }
+    }
+
+    override fun getUrFromUrlView(): String {
+        return binding.edtUrl.text.toString()
+    }
+
+    override fun getBodyFromBodyView(): String {
+        return binding.edtBody.text.toString()
     }
 
     override fun goneBodyViewVisibility() {
@@ -76,5 +160,33 @@ class MainActivity constructor(private val presenter: MainPresenter = MainPresen
 
     override fun visibleBodyViewVisibility() {
         binding.bodyLayout.visibility = View.VISIBLE
+    }
+
+    override fun progressShow() {
+        binding.btnSendRequest.visibility = View.GONE
+        binding.progressBar.visibility = View.VISIBLE
+    }
+
+    override fun progressDismiss() {
+        binding.progressBar.visibility = View.GONE
+        binding.btnSendRequest.visibility = View.VISIBLE
+    }
+
+    override fun snackbar(message: String, color: Int) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+            .setTextColor(ContextCompat.getColor(this, color))
+            .show()
+    }
+
+    override fun intentToDisplayActivity(responseEntity: ResponseEntity) {
+        val intent = Intent(baseContext, DisplayActivity::class.java)
+        intent.putExtra("RESPONSE_ENTITY", responseEntity)
+        startActivity(intent)
+    }
+
+    override fun intentToHistoryActivity(responseEntityList: ArrayList<ResponseEntity>) {
+        val intent = Intent(baseContext, HistoryActivity::class.java)
+        intent.putExtra("RESPONSES_HISTORY", responseEntityList)
+        startActivity(intent)
     }
 }
