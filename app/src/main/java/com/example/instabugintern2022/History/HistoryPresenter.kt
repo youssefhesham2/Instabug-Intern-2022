edc4a605db13915ff66a2db9a592791db8de9949
youssefhesham2 <youssef.hesham.rabea@gmail.com>
2022-06-02 13:20:39 +0200

    Handle Spinner Selected to filter the cached

--- app/src/main/java/com/example/instabugintern2022/History/HistoryPresenter.kt
+++ app/src/main/java/com/example/instabugintern2022/History/HistoryPresenter.kt
@@ -1,18 +1,41 @@
 package com.example.instabugintern2022.History
 
 import com.example.domain.entities.ResponseEntity
+import com.example.domain.usecases.FilterRequestUsesCase
 import com.example.instabugintern2022.displayactivity.DisplayView
+import java.text.FieldPosition
 
-class HistoryPresenter {
+class HistoryPresenter constructor(private val filterRequestUsesCase: FilterRequestUsesCase = FilterRequestUsesCase()) {
     private var view: HistoryView? = null
+    private var responseEntityList = ArrayList<ResponseEntity>()
     fun onAttached(view: HistoryView) {
         this.view = view
         getBundleData()
     }
 
     private fun getBundleData() {
-        var responseEntityList = view?.getBundleData()
+        view?.getBundleData()?.let { responseEntityList = it }
         responseEntityList?.let { view?.notifyDataSetChanged(it) }
+    }
+
+    fun onSpinnerSelectedItem(position: Int) {
+        when (position) {
+            0 -> responseEntityList?.let { view?.notifyDataSetChanged(it) }
+
+            1 -> view?.notifyDataSetChanged(
+                filterRequestUsesCase.filterRequestByType(
+                    0,
+                    responseEntityList
+                ) as ArrayList<ResponseEntity>
+            )
+
+            2 -> view?.notifyDataSetChanged(
+                filterRequestUsesCase.filterRequestByType(
+                    1,
+                    responseEntityList
+                ) as ArrayList<ResponseEntity>
+            )
+        }
     }
 
     fun onDestroy() {
